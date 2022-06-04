3dd20a49a094508e123916a3a7cdeaa6221bbf96
Neha2580 <91769030+Neha2580@users.noreply.github.com>
2022-06-04 15:08:39 +0530

    Time: 37 ms (38.49%), Space: 19.1 MB (98.03%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
+   
+    
     void moveZeroes(vector<int>& nums) {
-         stack<int> stk;
-        int count=0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==0){
-                count++;
+        int i=0, j=0; 
+        while(j<nums.size()){
+            if(nums[j]==0){
+                j++;
             }
-            else if(nums[i]!=0){
-                stk.push(nums[i]);
+            else{
+                swap(nums[i++], nums[j++]);
             }
         }
-        
-        while(count>0){
-            stk.push(0);
-            count--;
-        }
-        int j=0;
-        while(!stk.empty()){
-            nums[j]=stk.top();
-            stk.pop();
-            j++;
-        }
-      reverse(nums.begin(), nums.end());
     }
 };
