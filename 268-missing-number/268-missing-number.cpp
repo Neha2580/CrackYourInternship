6bfdc0eb2f87014f5ae65f53a225ea11974c177d
Neha2580 <91769030+Neha2580@users.noreply.github.com>
2022-06-22 01:03:42 +0530

    Time: 37 ms (25.58%), Space: 17.9 MB (64.11%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        
+        int ans=0;
+        for(int i=0; i<n; i++){
+            ans^=nums[i];
+        }
+        
+        for(int i=0; i<=n; i++){
+            ans^=i;
+        }
+        
+        return ans;
+    }
+};
