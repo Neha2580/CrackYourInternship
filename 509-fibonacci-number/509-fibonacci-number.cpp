df8733c3851662152fe7e639cedd740badad0c8b
Neha Sahu <91769030+Neha2580@users.noreply.github.com>
2022-07-06 11:10:38 +0530

    Time: 21 ms (17.74%), Space: 5.9 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fib(int n) {
+        int ans=0;
+        if(n==0){
+            return 0;
+        }
+        
+        if(n>1){
+            ans= fib(n-1)+fib(n-2);
+        }
+        else if(n==1){
+            return 1;
+        }
+        
+        return ans;
+        
+    }
+};
