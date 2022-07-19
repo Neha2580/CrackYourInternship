aa0ae728240643e9909c002c3ce14096229e692f
Neha Sahu <91769030+Neha2580@users.noreply.github.com>
2022-07-19 21:39:16 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (90.76%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     vector<vector<int>> generate(int n) {
         
-        vector<vector<int>> r(n);
+        vector<vector<int>> v(n);
         
         for(int i=0; i<n; i++){
-            r[i].resize(i+1);
-            r[i][0] = r[i][i] = 1;
+            v[i].resize(i+1);
+            v[i][0]=v[i][i]=1;
             
             for(int j=1; j<i; j++){
-                r[i][j] = r[i-1][j-1] + r[i-1][j];
+                v[i][j] = v[i-1][j-1] + v[i-1][j];
             }
         }
         
-        return r;
+        
+        return v;
+        
         
     }
 };
